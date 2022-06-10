3cabe19eac894d40612447d2ece843216376c3d7
thomasseo1 <t.seo8456@gmail.com>
2022-06-10 15:51:56 -0700

    added functionality for mutations in typedefs, resolvers, userControllers
    Co-authored-by: Maxwell Cook <maxwell.h.cook@gmail.com>

--- applyGQL.ts
+++ applyGQL.ts
@@ -1,14 +1,20 @@
 // deno-lint-ignore-file no-explicit-any
 // maybe go through the file and clean up TypeScript any types
-import { gql, graphql } from "https://deno.land/x/oak_graphql@0.6.3/deps.ts";
-import { ISettings, renderPlaygroundPage} from "https://deno.land/x/oak_graphql@0.6.3/graphql-playground-html/render-playground-html.ts";
-import { makeExecutableSchema } from "https://deno.land/x/oak_graphql@0.6.3/graphql-tools/schema/makeExecutableSchema.ts";
-import { fileUploadMiddleware, GraphQLUpload } from "https://deno.land/x/oak_graphql@0.6.3/fileUpload.ts";
-import { graphErrLibrary } from "./errorLibrary.ts";
-import { newErrors } from "./errorHandling/newErrors.ts"
+import { gql, graphql } from 'https://deno.land/x/oak_graphql@0.6.3/deps.ts';
+import {
+  ISettings,
+  renderPlaygroundPage,
+} from 'https://deno.land/x/oak_graphql@0.6.3/graphql-playground-html/render-playground-html.ts';
+import { makeExecutableSchema } from 'https://deno.land/x/oak_graphql@0.6.3/graphql-tools/schema/makeExecutableSchema.ts';
+import {
+  fileUploadMiddleware,
+  GraphQLUpload,
+} from 'https://deno.land/x/oak_graphql@0.6.3/fileUpload.ts';
+import { graphErrLibrary } from './errorLibrary.ts';
+import { newErrors } from './errorHandling/newErrors.ts';
 
 interface Constructable<T> {
-  new(...args: any): T & OakRouter;
+  new (...args: any): T & OakRouter;
 }
 
 interface OakRouter {
@@ -35,7 +41,7 @@ export interface ResolversProps {
 
 export async function applyGraphQL<T>({
   Router,
-  path = "/graphql",
+  path = '/graphql',
   typeDefs,
   resolvers,
   context,
@@ -49,7 +55,7 @@ export async function applyGraphQL<T>({
   augmentedTypeDefs.push(
     gql`
       scalar Upload
-    `,
+    `
   );
   if (Array.isArray(resolvers)) {
     if (resolvers.every((resolver) => !resolver.Upload)) {
@@ -67,23 +73,28 @@ export async function applyGraphQL<T>({
   });
 
   type Output = {
-    standardError?: string,
-    statusCode?: number,
-    graphErr?: string,
-    graphQLSpec?: string,
-  }
+    standardError?: string;
+    statusCode?: number;
+    graphErr?: string;
+    graphQLSpec?: string;
+  };
 
-  type OutputArray = Output[]
+  type OutputArray = Output[];
 
-  const errorHandler = (resBody: any) : Output[] => {
+  const errorHandler = (resBody: any): Output[] => {
     const output: OutputArray = [];
     // console.log('res body', resBody.errors[0].message)
     for (let j = 0; j < resBody.errors.length; j++) {
       // console.log(j);
       for (let i = 0; i < graphErrLibrary.length; i++) {
         // console.log(i);
-        console.log(graphErrLibrary[i].standardError, resBody.errors[j].message);
-        if (resBody.errors[j].message.startsWith(graphErrLibrary[i].standardError)) {
+        console.log(
+          graphErrLibrary[i].standardError,
+          resBody.errors[j].message
+        );
+        if (
+          resBody.errors[j].message.startsWith(graphErrLibrary[i].standardError)
+        ) {
           // possibly change later to return here instead to make more performant
           output.push(graphErrLibrary[i]);
         }
@@ -91,63 +102,65 @@ export async function applyGraphQL<T>({
     }
     // console.log(output);
     return output;
-  }
+  };
 
   await router.post(path, fileUploadMiddleware, async (ctx: any) => {
     const { response, request } = ctx;
-      // console.log("context98", ctx);
-      if (request.hasBody) {
-        try {
-          const contextResult = context ? await context(ctx) : undefined;
-          const body = ctx.params.operations || await request.body().value;
-          // console.log('body', await request.body().value)
-          // console.log("THIS", body.query);
-          const result = await (graphql as any)(
-            schema,
-            body.query,
-            resolvers,
-            contextResult,
-            body.variables || undefined,
-            body.operationName || undefined,
-          );
-          
-          response.body = result;
-          // console.log('RESPONSE', response.body);
-          
-          if (response.body.errors) {
-            const graphErrObj: OutputArray = errorHandler(response.body);
-            for (let i = 0; i < response.body.errors.length; i++) {
-              // We're not actually changing status behind the scenes
-              response.body.errors[i].statusCode = graphErrObj[i].statusCode;
-              response.body.errors[i].graphErr = graphErrObj[i].graphErr;
-              response.body.errors[i].graphQLSpec = graphErrObj[i].graphQLSpec;
-            }
-          } else {
-            // loop through all other response arrays 
-             for (const queryName in response.body.data) {
-              // check for null responses
-              if (response.body.data[queryName].length === 0) {
-                response.body.data[queryName].push({graphErr: newErrors(body.query, resolvers.Query)});
-              }
-            }
-            response.status = 200;
+    // console.log("context98", ctx);
+    if (request.hasBody) {
+      try {
+        const contextResult = context ? await context(ctx) : undefined;
+        const body = ctx.params.operations || (await request.body().value);
+        // console.log('body', await request.body().value)
+        // console.log("THIS", body.query);
+        const result = await (graphql as any)(
+          schema,
+          body.query,
+          resolvers,
+          contextResult,
+          body.variables || undefined,
+          body.operationName || undefined
+        );
+
+        response.body = result;
+        // console.log('RESPONSE', response.body);
+
+        if (response.body.errors) {
+          const graphErrObj: OutputArray = errorHandler(response.body);
+          for (let i = 0; i < response.body.errors.length; i++) {
+            // We're not actually changing status behind the scenes
+            response.body.errors[i].statusCode = graphErrObj[i].statusCode;
+            response.body.errors[i].graphErr = graphErrObj[i].graphErr;
+            response.body.errors[i].graphQLSpec = graphErrObj[i].graphQLSpec;
           }
-          return;
-        } catch (error) {
+        } else {
+          // loop through all other response arrays
+          for (const queryName in response.body.data) {
+            // check for null responses
+            console.log('TESTING');
+            if (response.body.data[queryName].length === 0) {
+              response.body.data[queryName].push({
+                graphErr: newErrors(body.query, resolvers.Query),
+              });
+            }
+          }
           response.status = 200;
-          response.body = {
-            data: null,
-            errors: [
-              {
-                message: error.message ? error.message : error,
-              },
-            ],
-          };
-          return;
         }
+        return;
+      } catch (error) {
+        response.status = 200;
+        response.body = {
+          data: null,
+          errors: [
+            {
+              message: error.message ? error.message : error,
+            },
+          ],
+        };
+        return;
       }
-    }, 
-  );
+    }
+  });
 
   await router.get(path, (ctx: any) => {
     console.log(extensions);
@@ -156,7 +169,7 @@ export async function applyGraphQL<T>({
       // perform more expensive content-type check only if necessary
       // XXX We could potentially move this logic into the GuiOptions lambda,
       // but I don't think it needs any overriding
-      const prefersHTML = request.accepts("text/html");
+      const prefersHTML = request.accepts('text/html');
 
       if (prefersHTML) {
         const playground = renderPlaygroundPage({
